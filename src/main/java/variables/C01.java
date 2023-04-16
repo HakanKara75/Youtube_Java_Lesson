b16467942d0637bf137da92af833317c9f2fd25d
HakanKara75 <hakanbatirhan@gmail.com>
2023-04-16 16:34:31 +0300

    v

---
+++ src/main/java/variables/C01.java
@@ -0,0 +1,60 @@
+package variables;
+
+public class C01 {
+    public static void main(String[] args) {
+
+        //Isminizin ilk harfini char data tipinde yazin
+        char isminIlkHarf= 'H';
+
+
+        //Oturdugun sehirin ilk harfini char data tipinde yazin
+        char sehrinIlkHarfi = 'H';
+
+        System.out.println(isminIlkHarf);
+
+    //"Calisiyor musunuz?" sorusunun cevabini boolean data tipinde yazin
+        boolean calisiyorMusun= true;
+
+        //"Evli misin?" sorusunun cevabini boolean data tipinde yazin
+        boolean evliMisin= false;
+        System.out.println(evliMisin);
+
+        //Ogrnecinin yasini byte data tipinde yazin
+        byte ogrencininYasi=12;
+
+        //Okuldaki ogrenci sayisini short data tipinde yazin
+        short okuldakiOgrenciSayisi= 548;
+
+        short minDeger= Short.MIN_VALUE;
+        System.out.println(minDeger);
+        //Turkiye'nin nufus sayisini int data tipinde yazin
+        int nufus= 85000000;
+
+        int maxDeger= Integer.MAX_VALUE;
+        System.out.println(maxDeger);
+        //Insan vucudundaki hucre sayisini long data tipinde yazin
+        long hucreSayisi= 21546846134835548L;
+
+        //Uzaydaki yıldız sayısı
+        long yildizSayisi= 354387765343876348L;
+
+        //Biskuvinin fiyatini float data tipinde yazin
+        float buskuvi= 5.99f;
+
+        //Kuyumcunun aldigi altinin olcum sonucunu double data tipinde yazin
+        double altinGrami= 0.354687684354868;
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/test/java/C01.java
@@ -0,0 +1,43 @@
+public class C01 {
+    public static void main(String[] args) {
+        //Isminizin ilk harfini char data tipinde yazin
+        char harf='A';
+
+        //Oturdugun sehirin ilk harfini char data tipinde yazin
+        char sehir='K';
+
+        //"Calisiyor musunuz?" sorusunun cevabini boolean data tipinde yazin
+        boolean calisiyorMusun= true;
+
+        //"Evli misin?" sorusunun cevabini boolean data tipinde yazin
+        boolean evliMisin= false;
+
+        //Ogrnecinin yasini byte data tipinde yazin
+        byte ogrenciYasi=12;
+
+
+        //Okuldaki ogrenci sayisini short data tipinde yazin
+        short ogrenciSayisi=756;
+        short minumumDeger= Short.MIN_VALUE;
+        System.out.println(minumumDeger);
+
+        //Turkiye'nin nufus sayisini int data tipinde yazin
+        int nufus= 85000000;
+        int maxDeger= Integer.MAX_VALUE;
+        System.out.println("Integer'in maximum degeri: " + maxDeger);
+
+        //Insan vucudundaki hucre sayisini long data tipinde yazin
+        long hucreSayisi= 5565866453548668621L;
+
+        //Biskuvinin fiyatini float data tipinde yazin
+        float biskuvi= 5.99f;
+
+        //Kuyumcunun aldigi altinin olcum sonucunu double data tipinde yazin
+        double altin= 0.35763587656;
+
+
+
+
+
+    }
+}
