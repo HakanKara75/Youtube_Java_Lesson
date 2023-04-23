890f604d6deb33088a9ecebec63ddec01ba24c00
HakanKara75 <hakanbatirhan@gmail.com>
2023-04-23 17:18:23 +0300

    v

---
+++ src/main/java/variables/C02.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class C02 {
+    String sayi= "45035903033464235";
+    String fiyat= "46456435634TL";
+    String fiyat1= "46456435634$";
+    String isim= "Hakan Kara";
+    String karakter ="H";
+    String karakter1 ="=";
+    String karakter2 ="&]+?";
+    String java= "Java, 1995 yılında Sun Microsystems tarafından yayınlanmış bir hesaplama platformu ve programlama dilidir.";
+
+
+}
